b54183233226bbb08425f4484ee94ddf17ba8b90
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-04 11:47:00 -0500

    ui refactoring

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -3,13 +3,40 @@
 import { FaArrowRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-export function LinkButton({ to, text }: { to: string; text: string }) {
+interface ButtonBaseI {
+  text: string;
+  link?: boolean;
+}
+
+interface ButtonI extends ButtonBaseI {
+  onClick: () => void;
+}
+
+interface LinkButtonI extends ButtonBaseI {
+  to: string;
+}
+
+export function ButtonBase({ text, link = false }: ButtonBaseI) {
+  return (
+    <div className="bg-secondary flex gap-2 items-center rounded-full py-2 px-4 min-w-[100px] hover:scale-105 hover:drop-shadow-bullet transition-all cursor-crosshair">
+      <span className="capitalize">{text}</span>
+      {link && <FaArrowRight className="text-background" />}
+    </div>
+  );
+}
+
+export function Button({ text, onClick }: ButtonI) {
+  return (
+    <div onClick={onClick}>
+      <ButtonBase text={text} />
+    </div>
+  );
+}
+
+export function LinkButton({ to, text }: LinkButtonI) {
   return (
     <Link to={to}>
-      <div className="bg-secondary flex gap-2 items-center rounded-full py-2 px-4 min-w-[100px] hover:scale-105 hover:drop-shadow-bullet transition-all cursor-crosshair">
-        <span className="capitalize">{text}</span>
-        <FaArrowRight className="text-background" />
-      </div>
+      <ButtonBase text={text} link={true} />
     </Link>
   );
 }
