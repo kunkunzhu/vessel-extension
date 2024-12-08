a02c3c5716c831bab472d6a528d1d92442ddd896
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-08 14:28:19 -0500

    update firebase config

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,10 @@ export default defineConfig({
           src: "public/background.js",
           dest: ".",
         },
+        {
+          src: "src/firebaseConfig.js",
+          dest: ".",
+        },
       ],
     }),
   ],
