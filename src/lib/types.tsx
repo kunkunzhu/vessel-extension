67e1671d94526de5fbe098fe5def38af999e486d
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-08 14:30:20 -0500

    add shared types

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,5 +1,13 @@
 /** @format */
 
+import { User } from "firebase/auth";
+
 export interface QueryMsgI {
   type: string;
+}
+
+export interface AuthResI {
+  success: boolean;
+  error?: any;
+  user?: User;
 }
